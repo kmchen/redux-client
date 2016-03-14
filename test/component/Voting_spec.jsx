c094b0517b4016e7c04b3c4168008c45b827c08e
kmchen <kmchen@chenkuaingdeair.htctaipei.htc.com.tw>
2016-03-14 17:58:27 +0800

    Add voting button check tests

--- test/component/Voting_spec.jsx
+++ test/component/Voting_spec.jsx
@@ -1,7 +1,8 @@
 import React    from 'react';
 import ReactDom from 'react-dom';
 import {renderIntoDocument,
-        scryRenderedDOMComponentsWithTag
+        scryRenderedDOMComponentsWithTag,
+        Simulate
 } from 'react-addons-test-utils';
 
 import Voting   from '../../src/components/Voting'
@@ -9,6 +10,9 @@ import {expect} from 'chai'
 
 describe('Voting', () => {
   it('renders a pair of buttons', () => {
+
+    //let callBack = (entry) => {};
+
     const component = renderIntoDocument(
       <Voting pair={['movie one', 'movie two']} />
     );
@@ -17,4 +21,14 @@ describe('Voting', () => {
     expect(buttons[0].textContent).to.equal('movie one');
     expect(buttons[1].textContent).to.equal('movie two');
   })
+  it('invokes callback when button is clicked', () => {
+    let value
+    let onClick = (entry) => value = entry;
+    const component = renderIntoDocument(
+      <Voting pair={['movie one', 'movie two']} onClick={onClick}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0])
+    expect(value).to.equal('movie one');
+  });
 });
