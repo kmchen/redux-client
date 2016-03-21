f25fada09e2b61101a323523f107ae80e5baad45
kmchen <kmchen@chenkuaingdeair.htctaipei.htc.com.tw>
2016-03-21 11:16:52 +0800

    Complete results page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React  from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('movie one', 'movie two');
+const tally = Map({'movie one': 3, 'movie two': 2});
+
 export default React.createClass({
   render: function(){
-            return React.cloneElement(this.props.children,{pair: pair});
+            return React.cloneElement(this.props.children,{pair: pair, tally: tally});
           }
 });
